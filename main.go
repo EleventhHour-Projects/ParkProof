6b7ae4b877b8f2c5e2b93583a3677ac6aa1260ec
Dhananjay Mishra <technicaldmcontact@gmail.com>
2025-12-30 22:19:04 +0530

    add qr code generation logic

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"app/internal"
+	"time"
 
+	"github.com/google/uuid"
+)
+
+func main() {
+	data := internal.VehicleQRCodeData{
+		Vehicle:     "DL1E5483",
+		TicketID:    uuid.New().String(),
+		EntryTime:   time.Now(),
+		IsExit:      false,
+		ParkingLot:  "PLID5387",
+		VehicleType: internal.VehicleTypeCar,
+	}
+	internal.QRCode(data)
 }
