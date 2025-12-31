638b969e73c0bb5a927a77d57243d4ee320e7fde
Dhananjay Mishra <technicaldmcontact@gmail.com>
2025-12-31 17:50:40 +0530

    qr and query endpoints

---
+++ internal/database/client.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+var queryCollection *mongo.Collection
+
+func MongoDB() {
+	uri := "fsaf"
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	client, err := mongo.Connect(options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI))
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatalf("Failed to ping database: %v", err)
+	}
+	coll := client.Database("parkproof_db").Collection("queries")
+	queryCollection = coll
+}
--- routes/api/query.go
+++ routes/api/query.go
@@ -1,9 +1,11 @@
 package api
 
 import (
+	"app/internal/database"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 )
 
 type QueryRrquest struct {
@@ -12,13 +14,29 @@ type QueryRrquest struct {
 	ResponseRequired bool      `json:"response_required"`
 	Time             time.Time `json:"time"`
 	WithInTime       int       `json:"with_in_time"`
+	Type             string    `json:"type"`
 }
 
 func SendQuery(c *fiber.Ctx) error {
 	var data QueryRrquest
 	c.BodyParser(&data)
 
-	//err := database.NewQuery(data)
+	q := database.Query{
+		Query:            data.Query,
+		ToParkingLot:     data.ToParkingLot,
+		ResponseRequired: data.ResponseRequired,
+		Time:             data.Time,
+		WithInTime:       data.WithInTime,
+		Status:           database.QueryStatusOpen,
+		Type:             data.Type,
+		ID:               "query" + uuid.New().String(),
+	}
+	err := database.AddQuery(q)
+	if err != nil {
+		c.Status(400)
+		return c.JSON(map[string]string{"error": err.Error()})
+	}
+
 	c.Status(200)
 	return c.JSON(map[string]string{"status": "done"})
 }
